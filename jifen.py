abf5a06f4502ab3da0b9cb9b7263cdd1f5f6ddc3
Jingge <1562024895@qq.com>
2020-12-25 10:40:17 +0800

    Update jifen.py

--- jifen.py
+++ jifen.py
@@ -1,71 +1,134 @@
 import requests
 
+import urllib.parse as urlparse
+
 user = ""
 password = ""
 sc_key = ""
+flag = "0"  # 上班卡0，下班卡1
 
 
-def punch():
-    token = get_token()
-    if not token:
-        print("登录失败!")
-        return "登录失败!"
-    url = "https://api.jifenzhi.com/attendance/api/workScheduleConfig/base/card?onOffFlag=0"
-    headers = {
-        "Host": "api.jifenzhi.com",
-        "Connection": "keep-alive",
-        "Pragma": "no-cache",
-        "Cache-Control": "no-cache",
-        "comomorgid": "15859861422091740001",
-        "Origin": "https://kq.jifenzhi.info",
-        "User-Agent": "Mozilla/5.0 (Linux; Android 5.1.1; MI 9 Build/NMF26X; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.136 Mobile Safari/537.36mpm24_android",
-        "authorization": "Bearer " + token,
-        "content-type": "application/json;charset=UTF-8",
-        "Accept": "*/*",
-        "Referer": "https://kq.jifenzhi.info/webFrontend/",
-        "Accept-Encoding": "gzip, deflate",
-        "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
-        "X-Requested-With": "com.jifenzhi.android"
-    }
-    response = requests.get(url, params=None, headers=headers).text
-    print(response)
-    if sc_key:
-        push_wx(sc_key, response)
+def get_url_query(url, query):
+    """
+    获取URL里面的参数值
+    """
+    queries = urlparse.parse_qs(url.split("?")[1])
+    if not queries.get(query):
+        return None
+    return "".join(queries.get(query))
 
 
-def get_token():
-    url = "https://auth.jifenzhi.info/oauth/token"
-    headers = {
-        "Connection": "keep - alive",
-        "Content-Type": "application/x-www-form-urlencoded",
-        "User-Agent": "okhttp/3.14.0",
-        "Authorization": "Basic bXBtMjRfYW5kcm9pZDpHOEo5NXQ1aWtMaXdEZnU5UWdQT0k5d3RsNVFvNTJ2cA=="
-    }
-    data = f"username=local%3A{user}&password={password}&grant_type=password"
-    response = requests.post(url, data=data, headers=headers).json()
+class JiFen:
+    def __init__(self, user, password, sc_key=""):
+        self.user = str(user)
+        self.password = str(password)
+        self.sc_key = sc_key
+        self.message = ""
+        self.user_info = {}
 
-    if response.get("access_token"):
-        token = response.get("access_token")
-        print("Got Token successfully:" + token)
-        return token
+        self.get_user_info()
 
+    def get_user_info(self):
+        url = "https://auth.jifenzhi.info/oauth/token"
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "User-Agent": "okhttp/3.14.0",
+            "Authorization": "Basic bXBtMjRfYW5kcm9pZDpHOEo5NXQ1aWtMaXdEZnU5UWdQT0k5d3RsNVFvNTJ2cA==",
+        }
+        data = f"username=local%3A{self.user}&password={self.password}&grant_type=password"
+        response = requests.post(url, data=data, headers=headers)
+        if response.status_code != 200:
+            raise Exception("账号/密码错误！")
+        else:
+            self.user_info = response.json()
 
-def push_wx(sc_key, desp=""):
-    server_url = f"https://sc.ftqq.com/{sc_key}.send"
-    params = {
-        "text": f'打卡成功',
-        "desp": desp
-    }
-    response = requests.get(server_url, params=params)
-    json_data = response.json()
-    if json_data['errno'] == 0:
-        print(f"推送成功。")
-    else:
-        print(f"推送失败：{json_data['errno']}({json_data['errmsg']})")
+    def punch(self, flag):
+        url = "https://api.jifenzhi.com/attendance/api/workScheduleConfig/base/card?onOffFlag=" + flag
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Linux; Android 5.1.1; MI 9 Build/NMF26X; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.136 Mobile Safari/537.36mpm24_android",
+            "authorization": "Bearer " + self.user_info["access_token"],
+            "comomorgid": "15859861422091740001",
+        }
+        response = requests.get(url, headers=headers).text
+        self.message += f"\n{response}"
+        print(response)
+
+    def read_announcement(self):  # 标记已读
+        unread_url_list = self._get_unread_announcement()
+        if not unread_url_list:
+            print("没有未读公告")
+            return
+        for unread_url in unread_url_list:
+            message_id = get_url_query(unread_url, "detail")
+            url = "https://pushcenter.jifenzhi.com/pushServer/siteUpdateStatus"
+            headers = {
+                "Authorization": "Bearer " + self.user_info["access_token"],
+                "User-Agent": "Mozilla/5.0 (Linux; Android 5.1.1; MI 9 Build/NMF26X; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.136 Mobile Safari/537.36mpm24_android",
+            }
+            data = f'{{"messageId":"{message_id}","msgType":2,"orgId":"15859861422091740001","memberId":"15859861448881740002","fromClientId":"mpm3.0,workflowx,odms,meritpoint,mpm-crm,taskClock,notice3,tasklist,ACTaskBusiness","userId":"{self.user_info["user_id"]}"}}'
+            response = requests.post(url, data=data, headers=headers).json()
+            if response.get("code") == "200":
+                msg = f"公告已阅读, id:{message_id}"
+                self.message += f"\n{msg}"
+                print(msg)
+                self._get_points(unread_url)
+
+    def _get_unread_announcement(self):  # 获取未读公告
+        unread_url_list = []
+        url = "https://pushcenter.jifenzhi.com/pushServer/pushResult"
+        params = {
+            "channel": "site",
+            "msgType": "2",
+            "pageSize": "5",
+            "pageNum": "1",
+            "fromClientId": "mpm3.0,workflowx,odms,meritpoint,mpm-crm,taskClock,notice3,tasklist,ACTaskBusiness",
+            "userId": self.user_info["user_id"],
+        }
+        response = requests.get(url, params=params).json()
+        if response.get("code") == "200":
+            for l in response["resultData"]["list"]:
+                if l["readStatus"] == 0:
+                    unread_url_list.append(l["click2url"])  # 'http://notice3.jifenzhi.info?detail=16049720071361100001'
+        return unread_url_list
+
+    def _get_points(self, unread_url):  # 获取阅读分
+        message_id = get_url_query(unread_url, "detail")
+        url = "https://api.jifenzhi.info/notice3/api/information/award?memberId=15859861448881740002&orgId=15859861422091740001&informationId=" + message_id
+        headers = {
+            "Authorization": "Bearer " + self.user_info["access_token"],
+            "User-Agent": "Mozilla/5.0 (Linux; Android 5.1.1; MI 9 Build/NMF26X; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.136 Mobile Safari/537.36mpm24_android",
+            "Content-Type": "application/json;charset=UTF-8",
+            "Referer": unread_url,
+        }
+        data = f'{{"memberId":"15859861448881740002","orgId":"15859861422091740001","informationId":"{message_id}"}}'
+        response = requests.post(url, data=data, headers=headers)
+        msg = f"获取公告阅读分, id:{message_id}, {response.text}"
+        self.message += f"\n{msg}"
+        print(msg)
+
+    def push_wx(self):
+        if not self.sc_key:
+            print("没有提供secret key.")
+            return
+        server_url = f"https://sc.ftqq.com/{sc_key}.send"
+        params = {
+            "text": f'打卡成功',
+            "desp": self.message
+        }
+        response = requests.get(server_url, params=params)
+        json_data = response.json()
+        if json_data['errno'] == 0:
+            print(f"推送成功。")
+        else:
+            print(f"推送失败：{json_data['errno']}({json_data['errmsg']})")
 
 
 if __name__ == '__main__':
     user = input()
     password = input()
+    flag = input()
     sc_key = input()
-    punch()
+    jf = JiFen(user, password, sc_key)
+    jf.punch(flag)
+    jf.read_announcement()
+    jf.push_wx()
